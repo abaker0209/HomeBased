fc7cbc5c4ba67131359ce50af9025a7245e75104
abaker0209 <amberbaker10519@gmail.com>
2026-05-04 19:59:49 -0400

    Add files via upload

--- database/schema.sql
+++ database/schema.sql
@@ -1,443 +1,117 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               12.2.2-MariaDB - MariaDB Server
--- Server OS:                    Win64
--- HeidiSQL Version:             12.14.0.7165
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+Updatecheck<|||>3<|||>1
+DoUsageStatistics<|||>3<|||>1
+UpdatecheckLastrun<|||>1<|||>2026-05-04 15:43:31
+LastUsageStatisticCall<|||>1<|||>2026-04-30 17:41:25
+ColWidths_connform.ListSessions<|||>1<|||>163,50,50,50,50,50,10
+ColsVisible_connform.ListSessions<|||>1<|||>0,1,4,6
+ColPositions_connform.ListSessions<|||>1<|||>0,1,2,3,4,5,6
+ColSort_connform.ListSessions<|||>1<|||>0,0
+MainWinOnMonitor<|||>3<|||>0
+MainWinWidth<|||>3<|||>974
+MainWinHeight<|||>3<|||>1039
+ColWidths_MainForm.ListDatabases<|||>1<|||>150,80,50,50,50,50,50,50,50,50,120
+ColsVisible_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColPositions_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColSort_MainForm.ListDatabases<|||>1<|||>0,0
+ColWidths_MainForm.ListVariables<|||>1<|||>160,200,316
+ColsVisible_MainForm.ListVariables<|||>1<|||>0,1,2
+ColPositions_MainForm.ListVariables<|||>1<|||>0,1,2
+ColSort_MainForm.ListVariables<|||>1<|||>0,0
+ColWidths_MainForm.ListStatus<|||>1<|||>160,316,100,100
+ColsVisible_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColPositions_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColSort_MainForm.ListStatus<|||>1<|||>0,0
+ColWidths_MainForm.ListProcesses<|||>1<|||>70,80,80,80,80,50,50,186
+ColsVisible_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColPositions_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColSort_MainForm.ListProcesses<|||>1<|||>0,1
+ColWidths_MainForm.ListCommandStats<|||>1<|||>120,100,100,100,256
+ColsVisible_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColPositions_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColSort_MainForm.ListCommandStats<|||>1<|||>1,1
+ColWidths_MainForm.ListTables<|||>1<|||>228,70,70,120,120,70,100,50,70,70,70,70,70,90,120,70,70,70,50
+ColsVisible_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,18
+ColPositions_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+ColSort_MainForm.ListTables<|||>1<|||>6,0
+LastSessions<|||>1<|||>HomeBased - Private
+LastActiveSession<|||>1<|||>HomeBased - Private
+CreateDbCollation<|||>1<|||>utf8mb4_uca1400_ai_ci
+ColWidths_frmTableEditor.listColumns<|||>1<|||>76,100,168,90,60,65,50,127,130,152,100,100,50,93,288
+ColsVisible_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14
+ColPositions_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14
+ColSort_frmTableEditor.listColumns<|||>1<|||>0,0
+ColWidths_frmTableEditor.treeIndexes<|||>1<|||>234,100,80,120,80
+ColsVisible_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3,4
+ColPositions_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3,4
+ColSort_frmTableEditor.treeIndexes<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listForeignKeys<|||>1<|||>196,80,100,80,80,84
+ColsVisible_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColPositions_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColSort_frmTableEditor.listForeignKeys<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listCheckConstraints<|||>1<|||>200,420
+ColsVisible_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColPositions_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColSort_frmTableEditor.listCheckConstraints<|||>1<|||>-1,0
+Usermanager_WindowWidth<|||>3<|||>705
+ColWidths_UserManagerForm.listUsers<|||>1<|||>96,80
+ColsVisible_UserManagerForm.listUsers<|||>1<|||>0,1
+ColPositions_UserManagerForm.listUsers<|||>1<|||>0,1
+ColSort_UserManagerForm.listUsers<|||>1<|||>0,0
+MainWinMaximized<|||>3<|||>1
+querymemoheight<|||>3<|||>737
+dbtreewidth<|||>3<|||>197
+MainWinLeft<|||>3<|||>858
+MainWinTop<|||>3<|||>22
+SessionManager_WindowLeft<|||>3<|||>993
+SessionManager_WindowTop<|||>3<|||>76
+ExportSQL_Filenames<|||>1<|||>C:\Users\owner\HomeBased\database\schema.sql<{{{><}}}>C:\Users\owner\OneDrive - Indiana University\Desktop\db.sql<{{{><}}}>schema.sql<{{{><}}}>
+ExportSQL_CreateDatabases<|||>3<|||>0
+ExportSQL_CreateTables<|||>3<|||>0
+SQLFile0<|||>1<|||>C:\Users\owner\AppData\Roaming\HeidiSQL\Backups\query-tab-2026-02-28_16-02-58-779.sql
+Servers\HomeBased - Private\SessionCreated<|||>1<|||>2026-02-16 19:10:16
+Servers\HomeBased - Private\Host<|||>1<|||>127.0.0.1
+Servers\HomeBased - Private\WindowsAuth<|||>3<|||>0
+Servers\HomeBased - Private\CleartextPluginEnabled<|||>3<|||>0
+Servers\HomeBased - Private\User<|||>1<|||>root
+Servers\HomeBased - Private\Password<|||>1<|||>8
+Servers\HomeBased - Private\LoginPrompt<|||>3<|||>1
+Servers\HomeBased - Private\Port<|||>1<|||>3306
+Servers\HomeBased - Private\NetType<|||>3<|||>2
+Servers\HomeBased - Private\Compressed<|||>3<|||>0
+Servers\HomeBased - Private\LocalTimeZone<|||>3<|||>0
+Servers\HomeBased - Private\QueryTimeout<|||>3<|||>30
+Servers\HomeBased - Private\KeepAlive<|||>3<|||>20
+Servers\HomeBased - Private\FullTableStatus<|||>3<|||>1
+Servers\HomeBased - Private\Databases<|||>1<|||>
+Servers\HomeBased - Private\Library<|||>1<|||>libmariadb.dll
+Servers\HomeBased - Private\Comment<|||>1<|||>
+Servers\HomeBased - Private\StartupScriptFilename<|||>1<|||>
+Servers\HomeBased - Private\TreeBackground<|||>3<|||>536870911
+Servers\HomeBased - Private\SSHtunnelActive<|||>3<|||>0
+Servers\HomeBased - Private\SshExecutable<|||>1<|||>ssh.exe
+Servers\HomeBased - Private\SSHtunnelHost<|||>1<|||>
+Servers\HomeBased - Private\SSHtunnelHostPort<|||>3<|||>22
+Servers\HomeBased - Private\SSHtunnelUser<|||>1<|||>owner
+Servers\HomeBased - Private\SSHtunnelPassword<|||>1<|||>497368727A683736363B276
+Servers\HomeBased - Private\SSHtunnelTimeout<|||>3<|||>4
+Servers\HomeBased - Private\SSHtunnelPrivateKey<|||>1<|||>
+Servers\HomeBased - Private\SSHtunnelPort<|||>3<|||>3306
+Servers\HomeBased - Private\SSL_Active<|||>3<|||>0
+Servers\HomeBased - Private\SSL_Key<|||>1<|||>
+Servers\HomeBased - Private\SSL_Cert<|||>1<|||>
+Servers\HomeBased - Private\SSL_CA<|||>1<|||>
+Servers\HomeBased - Private\SSL_Cipher<|||>1<|||>
+Servers\HomeBased - Private\SSL_Verification<|||>3<|||>2
+Servers\HomeBased - Private\IgnoreDatabasePattern<|||>1<|||>
+Servers\HomeBased - Private\LogFileDdl<|||>3<|||>0
+Servers\HomeBased - Private\LogFileDml<|||>3<|||>0
+Servers\HomeBased - Private\LogFilePath<|||>1<|||>C:\Users\owner\AppData\Roaming\HeidiSQL\Logs\%session\%db\%y%m%d.sql
+Servers\HomeBased - Private\RefusedCount<|||>3<|||>26
+Servers\HomeBased - Private\ServerVersionFull<|||>1<|||>12.2.2-MariaDB - MariaDB Server
+Servers\HomeBased - Private\ConnectCount<|||>3<|||>30
+Servers\HomeBased - Private\ServerVersion<|||>3<|||>120202
+Servers\HomeBased - Private\LastConnect<|||>1<|||>2026-05-04 15:43:37
+Servers\HomeBased - Private\lastUsedDB<|||>1<|||>homebased
+Servers\HomeBased - Private\FavoriteObjects<|||>1<|||>sys||host_summary<{{{><}}}>
+Servers\HomeBased - Private\homebased|item_summary\Sort<|||>1<|||>0_PurchDate|
+Servers\HomeBased - Private\QueryHistory\20<|||>1<|||>2026-05-02 12:42:37|homebased|0|INSERT INTO platforms (PlatformName, SiteAddress) <{{{><}}}>VALUES <{{{><}}}>	('eBay', 'eBay.com'), <{{{><}}}>	('Depop', 'Depop.com'),  <{{{><}}}>	('Mercari', 'Mercari.com'),  <{{{><}}}>	('FB Marketplace', 'facebook.com/marketplace'), <{{{><}}}>	('Poshmark', 'Poshmark.com')<{{{><}}}><{{{><}}}>; <{{{><}}}>	
